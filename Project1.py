dd9b69b9b8bef2f63cd15bd8cbc513ac7ba0f68b
LanzAndreiMolina <114982905+LanzAndreiMolina@users.noreply.github.com>
2023-07-06 13:57:08 +0800

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,18 @@
+print("Programmer: Lanz Andrei Molina")
+print("Date Created: July 6, 2023")
+print("Description of the Program: This is my first project in github.")
+print()
+print("First Name: Lanz Andrei")
+print("Middle Name: Tanedo")
+print("Last Name: Molina")
+print("Course: BS Information Technology")
+print("Year: 3rd year")
+print("Gender: Male")
+print("Civil Status: ")
+print("Nationality: Filipino")
+print()
+print("Motto: Action speaks louder than voice")
+print("PHINMA Araullo University")
+print("College: CITE")
+
+
---
+++ Project2.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 8
+
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+divd = num1 / num2
+
+print("First number: ", num1, "Second number: ", num2)
+print()
+print("The SUM is = {0}".format(add))
+print("The DIFFERENCE is = {0}".format(sub))
+print("The PRODUCT is = {0}".format(mult))
+print("The QUOTIENT is = {0}".format(divd))
